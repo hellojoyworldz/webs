659b8811caad7d264d23f6cba51861b6868e41b2
hellojoyworldz <hellojoyworldz@gmail.com>
2022-11-17 19:37:39 +0900

    layout finish 22.11.17 19:37

--- react_site2/src/App.js
+++ react_site2/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import Header from "./components/layout/Header";
 import Main from "./components/layout/Main";
 import Footer from "./components/layout/Footer";
-import Visual from "./components/section/Visual";
+//import Visual from "./components/section/Visual";
+import Slide from "./components/section/SwiperSlide";
 import Step from "./components/section/Step";
 import Gallery from "./components/section/Gallery";
 import Blog from "./components/section/Blog";
@@ -14,7 +15,7 @@ const App = () => {
     <>
       <Header />
       <Main>
-        <Visual />
+        <Slide />
         <Step />
         <Gallery />
         <Blog />
