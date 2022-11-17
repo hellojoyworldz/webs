09df0ec027148cf15b4118d112b2396c70951781
hellojoyworldz <hellojoyworldz@gmail.com>
2022-11-17 17:49:43 +0900

    22.11.17 17:49

--- react_site2/src/App.js
+++ react_site2/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/layout/Header";
 import Main from "./components/layout/Main";
 import Footer from "./components/layout/Footer";
-import SwiperSlide from "./components/section/SwiperSlide";
 import Visual from "./components/section/Visual";
 import Step from "./components/section/Step";
 import Gallery from "./components/section/Gallery";
@@ -16,7 +15,6 @@ const App = () => {
       <Header />
       <Main>
         <Visual />
-
         <Step />
         <Gallery />
         <Blog />
