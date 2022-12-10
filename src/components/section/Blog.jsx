b454f640b35fd75038603b27a8b32070b127415d
hellojoyworldz <hellojoyworldz@gmail.com>
2022-12-10 16:36:34 +0900

    first commit

---
+++ src/components/section/Blog.jsx
@@ -0,0 +1,173 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <section className="mainBlog">
+      <div className="in">
+        <div className="cm-main__header">
+          <h2 className="tit">Popular hotel in 서울</h2>
+          <p className="txt">
+            호텔스컴바인에서 모든 호텔과 숙소를 한 눈에 비교하세요
+          </p>
+        </div>
+        <ul className="mainBlog__list">
+          <li className="item">
+            <a href="#!">
+              <span className="thumb">
+                <img
+                  src="assets/img/mainBlog__img01.jpg"
+                  width="275"
+                  height="203"
+                  alt=""
+                />
+              </span>
+              <strong className="tit">글래드 여의도</strong>
+              <p className="txt">
+                글래드 호텔 여의도는 9호선 국회의사당역 4번 출구에서 도보 1분
+                거리에 있습니다. 여의도 공원이 도보로 5분 거리에 있으며, 'IFC
+                몰'과 '더 현대 서울'까지는 약 15분 정도 소요됩니다.
+              </p>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#!">
+              <span className="thumb">
+                <img
+                  src="assets/img/mainBlog__img02.jpg"
+                  width="275"
+                  height="203"
+                  alt=""
+                />
+              </span>
+              <strong className="tit">롯데 호텔 월드</strong>
+              <p className="txt">
+                2022년 5월 새롭게 오픈한 롯데 호텔 월드는 세계 최대 규모의 실내
+                놀이공원인 롯데월드 어드벤쳐와 롯데월드몰과 바로 연결되어
+                있습니다.
+              </p>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#!">
+              <span className="thumb">
+                <img
+                  src="assets/img/mainBlog__img03.jpg"
+                  width="275"
+                  height="203"
+                  alt=""
+                />
+              </span>
+              <strong className="tit">서울 신라 호텔</strong>
+              <p className="txt">
+                서울 신라 호텔은 3호선 동대입구역 5번 출구에서 도보로 5분 거리에
+                있으며 명동과 동대문 방면으로 무료 셔틀버스를 운행하고 있습니다.
+                호텔에서 명동 쇼핑 거리까지는 차로 15분, 강남역까지는 차로 약
+                20분이 소요됩니다.
+              </p>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#!">
+              <span className="thumb">
+                <img
+                  src="assets/img/mainBlog__img04.jpg"
+                  width="275"
+                  height="203"
+                  alt=""
+                />
+              </span>
+              <strong className="tit">그랜드 하얏트 서울</strong>
+              <p className="txt">
+                최고의 시설을 자랑하는 그랜드 하얏트 서울은 지하철 6호선
+                이태원역 2번 출구에서 도보로 15분 거리에 자리하고 있습니다.
+                주변의 관광 명소로는 다양한 문화가 어우러진 이태원과 문화 공연을
+                감상할 수 있는 블루스퀘어가 도보로 약 15분 거리에 있으며 명동이
+                차로 10분 거리에 자리하고 있습니다.
+              </p>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#!">
+              <span className="thumb">
+                <img
+                  src="assets/img/mainBlog__img05.jpg"
+                  width="275"
+                  height="203"
+                  alt=""
+                />
+              </span>
+              <strong className="tit">글래드 강남 코엑스센터</strong>
+              <p className="txt">
+                글래드 강남 코엑스센터는 지하철 2호선 삼성역 1번 출구에서 도보
+                1분 거리에 위치해 있습니다. 주변 관광지로는 쇼핑하기 좋은
+                코엑스몰이 도보 약 5분, 롯데월드가 차로 약 10분, 신사동
+                가로수길이 차로 약 20분 정도 소요됩니다.
+              </p>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#!">
+              <span className="thumb">
+                <img
+                  src="assets/img/mainBlog__img06.jpg"
+                  width="275"
+                  height="203"
+                  alt=""
+                />
+              </span>
+              <strong className="tit">신라스테이 광화문</strong>
+              <p className="txt">
+                신라스테이 광화문은 5호선 광화문역 2번 출구에서 도보로 5분
+                거리에 있습니다.가까운 도보 거리에 시청, 대사관, 서울 파이낸스
+                센터 등이 있어 비즈니스 여행객에게 편리한 위치를 자랑합니다.
+                뿐만 아니라 서울 4대 고궁, 북촌한옥마을과 명동까지 차로 약
+                10분이면 가실 수 있어 관광객분들께도 최적의 위치를 자랑합니다.
+              </p>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#!">
+              <span className="thumb">
+                <img
+                  src="assets/img/mainBlog__img07.jpg"
+                  width="275"
+                  height="203"
+                  alt=""
+                />
+              </span>
+              <strong className="tit">그랜드 하얏트 서울</strong>
+              <p className="txt">
+                최고의 시설을 자랑하는 그랜드 하얏트 서울은 지하철 6호선
+                이태원역 2번 출구에서 도보로 15분 거리에 자리하고 있습니다.
+                주변의 관광 명소로는 다양한 문화가 어우러진 이태원과 문화 공연을
+                감상할 수 있는 블루스퀘어가 도보로 약 15분 거리에 있으며 명동이
+                차로 10분 거리에 자리하고 있습니다.
+              </p>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#!">
+              <span className="thumb">
+                <img
+                  src="assets/img/mainBlog__img08.jpg"
+                  width="275"
+                  height="203"
+                  alt=""
+                />
+              </span>
+              <strong className="tit">글래드 강남 코엑스센터</strong>
+              <p className="txt">
+                래드 강남 코엑스센터는 지하철 2호선 삼성역 1번 출구에서 도보 1분
+                거리에 위치해 있습니다. 주변 관광지로는 쇼핑하기 좋은 코엑스몰이
+                도보 약 5분, 롯데월드가 차로 약 10분, 신사동 가로수길이 차로 약
+                20분 정도 소요됩니다.
+              </p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
---
+++ src/components/section/SwiperSlide.jsx
@@ -0,0 +1,57 @@
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const Slide = () => {
+  return (
+    <section className="visual">
+      <div className="in">
+        <div className="visual__header">
+          <h2 className="visual__tit">
+            HOTELS
+            <br />
+            COMBINED
+          </h2>
+          <p className="visual__txt">
+            너무 무리하지 말아요!
+            <br />
+            이미 당신은 해내고 있고
+            <br />
+            앞으로도 잘 할 수 있을거예요!
+          </p>
+          <span className="visual__view">
+            <a href="#!">더 알아보기</a>
+          </span>
+        </div>
+      </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}>
+        <SwiperSlide>
+          <img src="../assets/img/visual__img01.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../assets/img/visual__img02.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../assets/img/visual__img03.jpg" alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
+};
+
+export default Slide;
