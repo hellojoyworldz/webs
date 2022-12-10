7ece1c709eee1e318a3b9a571517102f0a726be4
hellojoyworldz <hellojoyworldz@gmail.com>
2022-12-10 16:27:08 +0900

    header github link update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -29,7 +29,7 @@ function Header(props) {
           </ul>
         </nav>
         <div className="header__member">
-          <a href="/">Github</a>
+          <a href="https://github.com/hellojoyworldz/react_site1">Github</a>
         </div>
         {/*
         <div className="header__ham">
