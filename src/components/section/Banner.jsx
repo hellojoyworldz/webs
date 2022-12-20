2009ebd08ec6deb23d14d4019f9f4b5c6816f5c7
hellojoyworldz <hellojoyworldz@gmail.com>
2022-12-20 09:06:16 +0900

    22.12.20 09:06

--- src/components/section/Banner.jsx
+++ src/components/section/Banner.jsx
@@ -5,7 +5,7 @@ function Banner({ fonts }) {
     <section id="bannerType" className={`banner__wrap ${fonts}`}>
       <h2 className="blind">배너 영역</h2>
       <div className="banner__inner">
-        <h3 className="title">WELCOME TO IDAHO</h3>
+        <h3 className="title">WELCOME TO Auckland</h3>
         <p className="desc">You can get more information via following page</p>
         <span className="small">view more</span>
       </div>
